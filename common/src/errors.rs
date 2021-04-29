bfa738ddaf955b164a61ae14e8ff3adcca3d5f6f
Irakliy Khaburzaniya <irakliyk@fb.com>
2021-04-28 20:29:56 -0700

    removed thiserror and displaydoc dependencies

--- common/src/errors.rs
+++ common/src/errors.rs
@@ -3,20 +3,37 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-use displaydoc::Display;
-use thiserror::Error;
+use core::fmt;
 
+// PROVER ERROR
+// ================================================================================================
 /// Represents an error thrown by the prover during an execution of the protocol
-#[derive(Debug, Display, Error)]
+#[derive(Debug, PartialEq)]
 pub enum ProverError {
-    /// A transition constraint was not satisfied at a certain step {0}
+    /// A transition constraint was not satisfied at step {0}
     UnsatisfiedTransitionConstraintError(usize),
-    /// The constraint polynomial's components do not all have the same degree, expected {0} but found {1}
+    /// The constraint polynomial's components do not all have the same degree; expected {0}, but was {1}
     MismatchedConstraintPolynomialDegree(usize, usize),
 }
 
+impl fmt::Display for ProverError {
+    #[rustfmt::skip]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UnsatisfiedTransitionConstraintError(step) => {
+                write!(f, "a transition constraint was not satisfied at step {}", step)
+            }
+            Self::MismatchedConstraintPolynomialDegree(expected, actual) => {
+                write!(f, "the constraint polynomial's components do not all have the same degree; expected {}, but was {}", expected, actual)
+            }
+        }
+    }
+}
+
+// VERIFIER ERROR
+// ================================================================================================
 /// Represents an error thrown by the verifier during an execution of the protocol
-#[derive(Debug, Display, Error)]
+#[derive(Debug, PartialEq)]
 pub enum VerifierError {
     /// Verification of low-degree proof failed: {0}
     FriVerificationFailed(fri::VerifierError),
@@ -36,15 +53,68 @@ pub enum VerifierError {
     ComputationContextDeserializationFailed,
 }
 
+impl fmt::Display for VerifierError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::FriVerificationFailed(err) => {
+                write!(f, "verification of low-degree proof failed: {}", err)
+            }
+            Self::TraceQueryDoesNotMatchCommitment => {
+                write!(f, "trace query did not match the commitment")
+            }
+            Self::TraceQueryDeserializationFailed => {
+                write!(f, "trace query deserialization failed")
+            }
+            Self::ConstraintQueryDoesNotMatchCommitment => {
+                write!(f, "constraint query did not match the commitment")
+            }
+            Self::ConstraintQueryDeserializationFailed => {
+                write!(f, "constraint query deserialization failed")
+            }
+            Self::QuerySeedProofOfWorkVerificationFailed => {
+                write!(f, "query seed proof-of-work verification failed")
+            }
+            Self::OodFrameDeserializationFailed => {
+                write!(f, "out-of-domain frame deserialization failed")
+            }
+            Self::ComputationContextDeserializationFailed => {
+                write!(f, "computation context deserialization failed")
+            }
+        }
+    }
+}
+
+// ASSERTION ERROR
+// ================================================================================================
 /// Represents an error thrown during evaluation
-#[derive(Debug, Display, Error, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum AssertionError {
-    /// expected trace width to be at least {0}, but was {1}
+    /// Expected trace width to be at least {0}, but was {1}
     TraceWidthTooShort(usize, usize),
-    /// expected trace length to be a power of two, but was {0}
+    /// Expected trace length to be a power of two, but was {0}
     TraceLengthNotPowerOfTwo(usize),
-    /// expected trace length to be at least {0}, but was {1}
+    /// Expected trace length to be at least {0}, but was {1}
     TraceLengthTooShort(usize, usize),
-    /// expected trace length to be exactly {0}, but was {1}
+    /// Expected trace length to be exactly {0}, but was {1}
     TraceLengthNotExact(usize, usize),
+}
+
+impl fmt::Display for AssertionError {
+    #[rustfmt::skip]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::TraceWidthTooShort(expected, actual) => {
+                write!(f, "expected trace width to be at least {}, but was {}", expected, actual)
+            }
+            Self::TraceLengthNotPowerOfTwo(actual) => {
+                write!(f, "expected trace length to be a power of two, but was {}", actual)
+            }
+            Self::TraceLengthTooShort(expected, actual) => {
+                write!(f, "expected trace length to be at least {}, but was {}", expected, actual)
+            }
+            Self::TraceLengthNotExact(expected, actual) => {
+                write!(f, "expected trace length to be exactly {}, but was {}", expected, actual)
+            }
+        }
+    }
 }
