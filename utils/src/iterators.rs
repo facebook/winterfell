ae7812f0a4177a069657c22df4d7772fa7d8a205
Irakliy Khaburzaniya <irakliyk@fb.com>
2021-06-03 08:24:12 -0700

    fixed header

--- utils/src/iterators.rs
+++ utils/src/iterators.rs
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 /// When `concurrent` feature is enabled, creates a parallel iterator; otherwise, creates a
 /// regular iterator. Optionally, `min_length` can be used to specify the minimum length of
 /// iterator to be processed in each thread.
