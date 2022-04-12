cf237b4a659fc115ccb284f0140f91110dc719af
Robin Salen <salenrobin@gmail.com>
2022-04-12 17:39:18 +0900

    [WIP] Add additional Rescue chain example to be used with RAPs

---
+++ examples/src/rescue_raps/air.rs
@@ -0,0 +1,162 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+use super::{
+    rescue::{self, STATE_WIDTH},
+    BaseElement, FieldElement, ProofOptions, CYCLE_LENGTH, TRACE_WIDTH,
+};
+use crate::utils::{are_equal, is_zero, not, EvaluationResult};
+use winterfell::{
+    Air, AirContext, Assertion, ByteWriter, EvaluationFrame, Serializable, TraceInfo,
+    TransitionConstraintDegree,
+};
+
+// CONSTANTS
+// ================================================================================================
+
+/// Specifies steps on which Rescue transition function is applied.
+const CYCLE_MASK: [BaseElement; CYCLE_LENGTH] = [
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ONE,
+    BaseElement::ZERO,
+    BaseElement::ZERO,
+];
+
+// RESCUE AIR
+// ================================================================================================
+
+pub struct PublicInputs {
+    pub seed: [BaseElement; 2],
+    pub result: [BaseElement; 2],
+}
+
+impl Serializable for PublicInputs {
+    fn write_into<W: ByteWriter>(&self, target: &mut W) {
+        target.write(&self.seed[..]);
+        target.write(&self.result[..]);
+    }
+}
+
+pub struct RescueAir {
+    context: AirContext<BaseElement>,
+    seed: [BaseElement; 2],
+    result: [BaseElement; 2],
+}
+
+impl Air for RescueAir {
+    type BaseField = BaseElement;
+    type PublicInputs = PublicInputs;
+
+    // CONSTRUCTOR
+    // --------------------------------------------------------------------------------------------
+    fn new(trace_info: TraceInfo, pub_inputs: PublicInputs, options: ProofOptions) -> Self {
+        let degrees =
+            vec![TransitionConstraintDegree::with_cycles(3, vec![CYCLE_LENGTH]); 2 * STATE_WIDTH];
+        assert_eq!(TRACE_WIDTH, trace_info.width());
+        RescueAir {
+            context: AirContext::new(trace_info, degrees, 4, options),
+            seed: pub_inputs.seed,
+            result: pub_inputs.result,
+        }
+    }
+
+    fn context(&self) -> &AirContext<Self::BaseField> {
+        &self.context
+    }
+
+    fn evaluate_transition<E: FieldElement + From<Self::BaseField>>(
+        &self,
+        frame: &EvaluationFrame<E>,
+        periodic_values: &[E],
+        result: &mut [E],
+    ) {
+        let current = frame.current();
+        let next = frame.next();
+        // expected state width is 2*4 field elements
+        debug_assert_eq!(TRACE_WIDTH, current.len());
+        debug_assert_eq!(TRACE_WIDTH, next.len());
+
+        // split periodic values into hash_flag and Rescue round constants
+        let hash_flag = periodic_values[0];
+        let ark = &periodic_values[1..];
+
+        // when hash_flag = 1, constraints for Rescue round are enforced
+        rescue::enforce_round(
+            &mut result[..STATE_WIDTH],
+            &current[..STATE_WIDTH],
+            &next[..STATE_WIDTH],
+            ark,
+            hash_flag,
+        );
+
+        rescue::enforce_round(
+            &mut result[STATE_WIDTH..],
+            &current[STATE_WIDTH..],
+            &next[STATE_WIDTH..],
+            ark,
+            hash_flag,
+        );
+
+        // when hash_flag = 0, constraints for copying hash values to the next
+        // step are enforced.
+        let copy_flag = not(hash_flag);
+        enforce_hash_copy(
+            &mut result[..STATE_WIDTH],
+            &current[..STATE_WIDTH],
+            &next[..STATE_WIDTH],
+            copy_flag,
+        );
+        enforce_hash_copy(
+            &mut result[STATE_WIDTH..],
+            &current[STATE_WIDTH..],
+            &next[STATE_WIDTH..],
+            copy_flag,
+        );
+    }
+
+    fn get_assertions(&self) -> Vec<Assertion<Self::BaseField>> {
+        // Assert starting and ending values of the hash chain
+        let last_step = self.trace_length() - 1;
+        vec![
+            // We start the hash chain on columns 0-3
+            Assertion::single(0, 0, self.seed[0]),
+            Assertion::single(1, 0, self.seed[1]),
+            // We end the hash chain on columns 4-7
+            Assertion::single(4, last_step, self.result[0]),
+            Assertion::single(5, last_step, self.result[1]),
+        ]
+    }
+
+    fn get_periodic_column_values(&self) -> Vec<Vec<Self::BaseField>> {
+        let mut result = vec![CYCLE_MASK.to_vec()];
+        result.append(&mut rescue::get_round_constants());
+        result
+    }
+}
+
+// HELPER EVALUATORS
+// ------------------------------------------------------------------------------------------------
+
+/// when flag = 1, enforces that the next state of the computation is defined like so:
+/// - the first two registers are equal to the values from the previous step
+/// - the other two registers are equal to 0
+fn enforce_hash_copy<E: FieldElement>(result: &mut [E], current: &[E], next: &[E], flag: E) {
+    result.agg_constraint(0, flag, are_equal(current[0], next[0]));
+    result.agg_constraint(1, flag, are_equal(current[1], next[1]));
+    result.agg_constraint(2, flag, is_zero(next[2]));
+    result.agg_constraint(3, flag, is_zero(next[3]));
+}
---
+++ examples/src/rescue_raps/prover.rs
@@ -0,0 +1,92 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+use super::{
+    rescue::{self, STATE_WIDTH},
+    BaseElement, FieldElement, ProofOptions, Prover, PublicInputs, RescueAir, Trace, TraceTable,
+    CYCLE_LENGTH, NUM_HASH_ROUNDS,
+};
+
+// RESCUE PROVER
+// ================================================================================================
+
+pub struct RescueProver {
+    options: ProofOptions,
+}
+
+impl RescueProver {
+    pub fn new(options: ProofOptions) -> Self {
+        Self { options }
+    }
+
+    pub fn build_trace(
+        &self,
+        seed: [BaseElement; 2],
+        result: ([BaseElement; 2], [BaseElement; 2]),
+        iterations: usize,
+    ) -> TraceTable<BaseElement> {
+        // allocate memory to hold the trace table
+        let trace_length = iterations * CYCLE_LENGTH;
+        let mut trace = TraceTable::new(2 * STATE_WIDTH, trace_length);
+
+        trace.fill(
+            |state| {
+                // initialize first state of the computation
+                state[0] = seed[0];
+                state[1] = seed[1];
+                state[2] = BaseElement::ZERO;
+                state[3] = BaseElement::ZERO;
+
+                // initialize intermediary state of the computation
+                state[4] = result.0[0];
+                state[5] = result.0[1];
+                state[6] = BaseElement::ZERO;
+                state[7] = BaseElement::ZERO;
+            },
+            |step, state| {
+                // execute the transition function for all steps
+                //
+                // for the first 14 steps in every cycle, compute a single round of
+                // Rescue hash; for the remaining 2 rounds, just carry over the values
+                // in the first two registers of the two chains to the next step
+                if (step % CYCLE_LENGTH) < NUM_HASH_ROUNDS {
+                    rescue::apply_round_parallel(state, step);
+                } else {
+                    state[2] = BaseElement::ZERO;
+                    state[3] = BaseElement::ZERO;
+
+                    state[6] = BaseElement::ZERO;
+                    state[7] = BaseElement::ZERO;
+                }
+            },
+        );
+
+        debug_assert_eq!(trace.get(0, trace_length - 1), trace.get(4, 0));
+        debug_assert_eq!(trace.get(1, trace_length - 1), trace.get(5, 0));
+
+        debug_assert_eq!(trace.get(4, trace_length - 1), result.1[0]);
+        debug_assert_eq!(trace.get(5, trace_length - 1), result.1[1]);
+
+        trace
+    }
+}
+
+impl Prover for RescueProver {
+    type BaseField = BaseElement;
+    type Air = RescueAir;
+    type Trace = TraceTable<BaseElement>;
+
+    fn get_pub_inputs(&self, trace: &Self::Trace) -> PublicInputs {
+        let last_step = trace.length() - 1;
+        PublicInputs {
+            seed: [trace.get(0, 0), trace.get(1, 0)],
+            result: [trace.get(4, last_step), trace.get(5, last_step)],
+        }
+    }
+
+    fn options(&self) -> &ProofOptions {
+        &self.options
+    }
+}
