c5405c285c40eff126c9180d71ac213b2c090796
Max Gillett <max.gillett@gmail.com>
2022-07-03 17:39:39 -0400

    refactored prover Matrix struct into Table

--- prover/src/table.rs
+++ prover/src/table.rs
@@ -1,36 +1,33 @@
 use super::StarkDomain;
-use core::{iter::FusedIterator, slice};
+use core::iter::FusedIterator;
+use core::ops::{Deref, DerefMut};
 use crypto::{ElementHasher, MerkleTree};
-use math::{fft, polynom, FieldElement};
-use utils::{batch_iter_mut, collections::Vec, iter, iter_mut, uninit_vector};
+use math::{fft, polynom, FieldElement, Matrix, RowMajorTable};
+use utils::{batch_iter_mut, collections::Vec, uninit_vector};
 
-#[cfg(feature = "concurrent")]
-use utils::iterators::*;
-
-// MATRIX
-// ================================================================================================
-
-/// A two-dimensional matrix of field elements arranged in column-major order.
-///
-/// This struct is used as a backing type for many objects manipulated by the prover. The matrix
-/// itself does not assign any contextual meaning to the values stored in it. For example, columns
-/// may contain evaluations of polynomials, or polynomial coefficients, or really anything else.
-/// However, the matrix does expose a number of methods which make assumptions about the underlying
-/// data.
-///
-/// A matrix imposes the following restrictions on its content:
-/// - A matrix must consist of at least 1 column and at least 2 rows.
-/// - All columns must be of the same length.
-/// - Number of rows must be a power of two.
 #[derive(Debug, Clone)]
-pub struct Matrix<E: FieldElement> {
-    columns: Vec<Vec<E>>,
+pub struct Table<E: FieldElement> {
+    pub table: RowMajorTable<E>,
 }
 
-impl<E: FieldElement> Matrix<E> {
+impl<E: FieldElement> Deref for Table<E> {
+    type Target = RowMajorTable<E>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.table
+    }
+}
+
+impl<E: FieldElement> DerefMut for Table<E> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.table
+    }
+}
+
+impl<E: FieldElement> Table<E> {
     // CONSTRUCTOR
     // --------------------------------------------------------------------------------------------
-    /// Returns a new [Matrix] instantiated with the data from the specified columns.
+    /// Returns a new [Table] instantiated with the data from the specified columns.
     ///
     /// # Panics
     /// Panics if:
@@ -60,79 +57,44 @@ impl<E: FieldElement> Matrix<E> {
             );
         }
 
-        Self { columns }
-    }
-
-    // PUBLIC ACCESSORS
-    // --------------------------------------------------------------------------------------------
-
-    /// Returns the number of columns in this matrix.
-    pub fn num_cols(&self) -> usize {
-        self.columns.len()
-    }
-
-    /// Returns the number of rows in this matrix.
-    pub fn num_rows(&self) -> usize {
-        self.columns[0].len()
-    }
-
-    /// Returns the element located at the specified column and row indexes in this matrix.
-    ///
-    /// # Panics
-    /// Panics if either `col_idx` or `row_idx` are out of bounds for this matrix.
-    pub fn get(&self, col_idx: usize, row_idx: usize) -> E {
-        self.columns[col_idx][row_idx]
-    }
-
-    /// Set the cell in this matrix at the specified column and row indexes to the provided value.
-    ///
-    /// # Panics
-    /// Panics if either `col_idx` or `row_idx` are out of bounds for this matrix.
-    pub fn set(&mut self, col_idx: usize, row_idx: usize, value: E) {
-        self.columns[col_idx][row_idx] = value;
-    }
-
-    /// Returns a reference to the column at the specified index.
-    pub fn get_column(&self, col_idx: usize) -> &[E] {
-        &self.columns[col_idx]
-    }
-
-    /// Returns a reference to the column at the specified index.
-    pub fn get_column_mut(&mut self, col_idx: usize) -> &mut [E] {
-        &mut self.columns[col_idx]
+        Self {
+            table: RowMajorTable::from_columns(columns),
+        }
     }
 
     /// Copies values of all columns at the specified row into the specified row slice.
     ///
     /// # Panics
-    /// Panics if `row_idx` is out of bounds for this matrix.
+    /// Panics if `row_idx` is out of bounds for this table.
     pub fn read_row_into(&self, row_idx: usize, row: &mut [E]) {
-        for (column, value) in self.columns.iter().zip(row.iter_mut()) {
-            *value = column[row_idx];
+        for (table_value, value) in self.table.get_row(row_idx).iter().zip(row.iter_mut()) {
+            *value = *table_value
         }
     }
 
-    /// Updates a row in this matrix at the specified index to the provided data.
+    /// Updates a row in this table at the specified index to the provided data.
     ///
     /// # Panics
-    /// Panics if `row_idx` is out of bounds for this matrix.
+    /// Panics if `row_idx` is out of bounds for this table.
     pub fn update_row(&mut self, row_idx: usize, row: &[E]) {
-        for (column, &value) in self.columns.iter_mut().zip(row) {
-            column[row_idx] = value;
+        for (table_value, value) in self.table.get_row_mut(row_idx).iter_mut().zip(row) {
+            *table_value = *value
         }
     }
 
-    // ITERATION
-    // --------------------------------------------------------------------------------------------
-
-    /// Returns an iterator over the columns of this matrix.
-    pub fn columns(&self) -> ColumnIter<E> {
-        ColumnIter::new(self)
+    /// Returns the columns of this matrix as a list of vectors.
+    pub fn into_columns(&self) -> Vec<Vec<E>> {
+        (0..self.num_cols()).map(|n| self.into_column(n)).collect()
     }
 
-    /// Returns a mutable iterator over the columns of this matrix.
-    pub fn columns_mut(&mut self) -> ColumnIterMut<E> {
-        ColumnIterMut::new(self)
+    /// Returns the column at the specified index.
+    pub fn into_column(&self, index: usize) -> Vec<E> {
+        self.data()
+            .iter()
+            .copied()
+            .skip(index)
+            .step_by(self.num_cols())
+            .collect()
     }
 
     // POLYNOMIAL METHODS
@@ -152,7 +114,7 @@ impl<E: FieldElement> Matrix<E> {
         let inv_twiddles = fft::get_inv_twiddles::<E::BaseField>(self.num_rows());
         // TODO: get ride of cloning by introducing another version of fft::interpolate_poly()
         let mut result = self.clone();
-        iter_mut!(result.columns).for_each(|column| fft::interpolate_poly(column, &inv_twiddles));
+        fft::matrix::interpolate_poly(&mut result.table, &inv_twiddles);
         result
     }
 
@@ -168,7 +130,7 @@ impl<E: FieldElement> Matrix<E> {
     ///   coefficients of a degree `num_rows - 1` polynomial.
     pub fn interpolate_columns_into(mut self) -> Self {
         let inv_twiddles = fft::get_inv_twiddles::<E::BaseField>(self.num_rows());
-        iter_mut!(self.columns).for_each(|column| fft::interpolate_poly(column, &inv_twiddles));
+        fft::matrix::interpolate_poly(&mut self.table, &inv_twiddles);
         self
     }
 
@@ -183,17 +145,13 @@ impl<E: FieldElement> Matrix<E> {
     ///   subgroup as well as the domain offset (to define a coset).
     /// * The resulting evaluations are returned in a new Matrix.
     pub fn evaluate_columns_over(&self, domain: &StarkDomain<E::BaseField>) -> Self {
-        let columns = iter!(self.columns)
-            .map(|poly| {
-                fft::evaluate_poly_with_offset(
-                    poly,
-                    domain.trace_twiddles(),
-                    domain.offset(),
-                    domain.trace_to_lde_blowup(),
-                )
-            })
-            .collect();
-        Self { columns }
+        let table = fft::matrix::evaluate_poly_with_offset(
+            &self.table,
+            domain.trace_twiddles(),
+            domain.offset(),
+            domain.trace_to_lde_blowup(),
+        );
+        Self { table }
     }
 
     /// Evaluates polynomials contained in the columns of this matrix at a single point `x`.
@@ -201,7 +159,10 @@ impl<E: FieldElement> Matrix<E> {
     where
         F: FieldElement + From<E>,
     {
-        iter!(self.columns).map(|p| polynom::eval(p, x)).collect()
+        self.into_columns()
+            .iter()
+            .map(|p| polynom::eval(p, x))
+            .collect()
     }
 
     // COMMITMENTS
@@ -220,7 +181,7 @@ impl<E: FieldElement> Matrix<E> {
         H: ElementHasher<BaseField = E::BaseField>,
     {
         // allocate vector to store row hashes
-        let mut row_hashes = unsafe { uninit_vector::<H::Digest>(self.num_rows()) };
+        let mut row_hashes = unsafe { uninit_vector::<H::Digest>(self.table.num_rows()) };
 
         // iterate though matrix rows, hashing each row; the hashing is done by first copying a
         // row into row_buf to avoid heap allocations, and then by applying the hash function to
@@ -229,7 +190,7 @@ impl<E: FieldElement> Matrix<E> {
             &mut row_hashes,
             128, // min batch size
             |batch: &mut [H::Digest], batch_offset: usize| {
-                let mut row_buf = vec![E::ZERO; self.num_cols()];
+                let mut row_buf = vec![E::ZERO; self.table.num_cols()];
                 for (i, row_hash) in batch.iter_mut().enumerate() {
                     self.read_row_into(i + batch_offset, &mut row_buf);
                     *row_hash = H::hash_elements(&row_buf);
@@ -240,40 +201,30 @@ impl<E: FieldElement> Matrix<E> {
         // build Merkle tree out of hashed rows
         MerkleTree::new(row_hashes).expect("failed to construct trace Merkle tree")
     }
-
-    // CONVERSIONS
-    // --------------------------------------------------------------------------------------------
-
-    /// Returns the columns of this matrix as a list of vectors.
-    ///
-    /// TODO: replace this with an iterator.
-    pub fn into_columns(self) -> Vec<Vec<E>> {
-        self.columns
-    }
 }
 
 // COLUMN ITERATOR
 // ================================================================================================
 
-pub struct ColumnIter<'a, E: FieldElement> {
-    matrix: &'a Matrix<E>,
+pub struct ColIterator<'a, E: FieldElement> {
+    matrix: &'a Table<E>,
     cursor: usize,
 }
 
-impl<'a, E: FieldElement> ColumnIter<'a, E> {
-    pub fn new(matrix: &'a Matrix<E>) -> Self {
+impl<'a, E: FieldElement> ColIterator<'a, E> {
+    pub fn new(matrix: &'a Table<E>) -> Self {
         Self { matrix, cursor: 0 }
     }
 }
 
-impl<'a, E: FieldElement> Iterator for ColumnIter<'a, E> {
-    type Item = &'a [E];
+impl<'a, E: FieldElement + 'a> Iterator for ColIterator<'a, E> {
+    type Item = Vec<E>;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.matrix.num_cols() - self.cursor {
             0 => None,
             _ => {
-                let column = self.matrix.get_column(self.cursor);
+                let column = self.matrix.into_column(self.cursor);
                 self.cursor += 1;
                 Some(column)
             }
@@ -281,67 +232,25 @@ impl<'a, E: FieldElement> Iterator for ColumnIter<'a, E> {
     }
 }
 
-impl<'a, E: FieldElement> ExactSizeIterator for ColumnIter<'a, E> {
+impl<'a, E: FieldElement + 'a> ExactSizeIterator for ColIterator<'a, E> {
     fn len(&self) -> usize {
         self.matrix.num_cols()
     }
 }
 
-impl<'a, E: FieldElement> FusedIterator for ColumnIter<'a, E> {}
-
-// MUTABLE COLUMN ITERATOR
-// ================================================================================================
-
-pub struct ColumnIterMut<'a, E: FieldElement> {
-    matrix: &'a mut Matrix<E>,
-    cursor: usize,
-}
-
-impl<'a, E: FieldElement> ColumnIterMut<'a, E> {
-    pub fn new(matrix: &'a mut Matrix<E>) -> Self {
-        Self { matrix, cursor: 0 }
-    }
-}
-
-impl<'a, E: FieldElement> Iterator for ColumnIterMut<'a, E> {
-    type Item = &'a mut [E];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.matrix.num_cols() - self.cursor {
-            0 => None,
-            _ => {
-                let column = self.matrix.get_column_mut(self.cursor);
-                self.cursor += 1;
-
-                // this is needed to get around mutable iterator lifetime issues; this is safe
-                // because the iterator can never yield a reference to the same column twice
-                let p = column.as_ptr();
-                let len = column.len();
-                Some(unsafe { slice::from_raw_parts_mut(p as *mut E, len) })
-            }
-        }
-    }
-}
-
-impl<'a, E: FieldElement> ExactSizeIterator for ColumnIterMut<'a, E> {
-    fn len(&self) -> usize {
-        self.matrix.num_cols()
-    }
-}
-
-impl<'a, E: FieldElement> FusedIterator for ColumnIterMut<'a, E> {}
+impl<'a, E: FieldElement> FusedIterator for ColIterator<'a, E> {}
 
 // MULTI-MATRIX COLUMN ITERATOR
 // ================================================================================================
 
-pub struct MultiColumnIter<'a, E: FieldElement> {
-    matrixes: &'a [Matrix<E>],
+pub struct MultiColIterator<'a, E: FieldElement> {
+    matrixes: &'a [Table<E>],
     m_cursor: usize,
     c_cursor: usize,
 }
 
-impl<'a, E: FieldElement> MultiColumnIter<'a, E> {
-    pub fn new(matrixes: &'a [Matrix<E>]) -> Self {
+impl<'a, E: FieldElement> MultiColIterator<'a, E> {
+    pub fn new(matrixes: &'a [Table<E>]) -> Self {
         // make sure all matrixes have the same number of rows
         if !matrixes.is_empty() {
             let num_rows = matrixes[0].num_rows();
@@ -362,8 +271,8 @@ impl<'a, E: FieldElement> MultiColumnIter<'a, E> {
     }
 }
 
-impl<'a, E: FieldElement> Iterator for MultiColumnIter<'a, E> {
-    type Item = &'a [E];
+impl<'a, E: FieldElement> Iterator for MultiColIterator<'a, E> {
+    type Item = Vec<E>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.matrixes.is_empty() {
@@ -373,7 +282,7 @@ impl<'a, E: FieldElement> Iterator for MultiColumnIter<'a, E> {
         match matrix.num_cols() - self.c_cursor {
             0 => None,
             _ => {
-                let column = matrix.get_column(self.c_cursor);
+                let column = matrix.into_column(self.c_cursor);
                 self.c_cursor += 1;
                 if self.c_cursor == matrix.num_cols() && self.m_cursor < self.matrixes.len() - 1 {
                     self.m_cursor += 1;
@@ -385,10 +294,10 @@ impl<'a, E: FieldElement> Iterator for MultiColumnIter<'a, E> {
     }
 }
 
-impl<'a, E: FieldElement> ExactSizeIterator for MultiColumnIter<'a, E> {
+impl<'a, E: FieldElement> ExactSizeIterator for MultiColIterator<'a, E> {
     fn len(&self) -> usize {
         self.matrixes.iter().fold(0, |s, m| s + m.num_cols())
     }
 }
 
-impl<'a, E: FieldElement> FusedIterator for MultiColumnIter<'a, E> {}
+impl<'a, E: FieldElement> FusedIterator for MultiColIterator<'a, E> {}
