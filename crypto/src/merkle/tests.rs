0b76b681e043e1a3005e094bc053073e90deb803
Al-Kindi-0 <oamine0101@gmail.com>
2022-08-22 09:39:36 +0200

    feat: unbatch BatchMerkleProof into individual Proof
    fix: format + naming
    
    fix: optimize by using a BTreeMap
    
    fix: signature into_paths
    
    fix: formatting
    
    feat: Merkle proof unbatching
    
    feat: Merkle proof unbatching 2
    
    feat: Merkle proof unbatching 2

--- crypto/src/merkle/tests.rs
+++ crypto/src/merkle/tests.rs
@@ -232,6 +232,30 @@ fn verify_batch() {
     assert!(MerkleTree::verify_batch(tree.root(), &[0, 1, 2, 3, 4, 5, 6, 7], &proof).is_ok());
 }
 
+#[test]
+fn verify_into_paths() {
+    let leaves = Digest256::bytes_as_digests(&LEAVES8).to_vec();
+    let tree = MerkleTree::<Blake3_256>::new(leaves).unwrap();
+
+    let proof1 = tree.prove(1).unwrap();
+    let proof2 = tree.prove(2).unwrap();
+    let proof1_2 = tree.prove_batch(&[1, 2]).unwrap();
+    let result = proof1_2.into_paths(&[1, 2]).unwrap();
+
+    assert_eq!(proof1, result[0]);
+    assert_eq!(proof2, result[1]);
+
+    let proof3 = tree.prove(3).unwrap();
+    let proof4 = tree.prove(4).unwrap();
+    let proof6 = tree.prove(5).unwrap();
+    let proof3_4_6 = tree.prove_batch(&[3, 4, 5]).unwrap();
+    let result = proof3_4_6.into_paths(&[3, 4, 5]).unwrap();
+
+    assert_eq!(proof3, result[0]);
+    assert_eq!(proof4, result[1]);
+    assert_eq!(proof6, result[2]);
+}
+
 proptest! {
     #[test]
     fn prove_n_verify(tree in random_blake3_merkle_tree(128),
@@ -268,6 +292,24 @@ proptest! {
         let proof2 = BatchMerkleProof::from_paths(&paths, &indices);
 
         prop_assert!(proof1 == proof2);
+    }
+
+    #[test]
+    fn into_paths(tree in random_blake3_merkle_tree(32),
+                      proof_indices in prop::collection::vec(any::<prop::sample::Index>(), 1..30)
+    )  {
+        let mut indices: Vec<usize> = proof_indices.iter().map(|idx| idx.index(32)).collect();
+        indices.sort_unstable(); indices.dedup();
+        let proof1 = tree.prove_batch(&indices[..]).unwrap();
+
+        let mut paths_expected = Vec::new();
+        for &idx in indices.iter() {
+            paths_expected.push(tree.prove(idx).unwrap());
+        }
+
+        let paths = proof1.into_paths(&indices);
+
+        prop_assert!(paths_expected == paths.unwrap());
     }
 }
 
