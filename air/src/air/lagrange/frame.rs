9a190d42fd963b414bae8e48440a832fcd15439f
Philippe Laferriere <plafer@protonmail.com>
2024-03-21 09:50:54 -0400

    from_lagrange_kernel_column_poly: use uninit_vector

--- air/src/air/lagrange/frame.rs
+++ air/src/air/lagrange/frame.rs
@@ -1,5 +1,6 @@
 use alloc::vec::Vec;
 use math::{polynom, FieldElement, StarkField};
+use utils::uninit_vector;
 
 /// The evaluation frame for the Lagrange kernel.
 ///
@@ -35,18 +36,19 @@ impl<E: FieldElement> LagrangeKernelEvaluationFrame<E> {
         let log_trace_len = lagrange_kernel_col_poly.len().ilog2();
         let g = E::from(E::BaseField::get_root_of_unity(log_trace_len));
 
-        let mut frame = Vec::with_capacity(log_trace_len as usize + 1);
+        let frame_len = log_trace_len as usize + 1;
+        let mut frame = unsafe { uninit_vector(frame_len) };
 
         // push c(x)
-        frame.push(polynom::eval(lagrange_kernel_col_poly, z));
+        frame[0] = polynom::eval(lagrange_kernel_col_poly, z);
 
         // push c(z * g), c(z * g^2), c(z * g^4), ..., c(z * g^(2^(v-1)))
         let mut g_exp = g;
-        for _ in 0..log_trace_len {
+        for idx in 1..frame_len {
             let x = g_exp * z;
             let lagrange_poly_at_x = polynom::eval(lagrange_kernel_col_poly, x);
 
-            frame.push(lagrange_poly_at_x);
+            frame[idx] = lagrange_poly_at_x;
 
             // takes on the values `g`, `g^2`, `g^4`, `g^8`, ...
             g_exp *= g_exp;
