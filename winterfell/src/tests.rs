492f247c8f86c3f9fd094d9e7ce38b7c5debd6fb
Al-Kindi-0 <82364884+Al-Kindi-0@users.noreply.github.com>
2024-08-12 14:58:32 +0200

    wip

--- winterfell/src/tests.rs
+++ winterfell/src/tests.rs
@@ -248,6 +248,231 @@ impl LogUpGkrEvaluator for PlainLogUpGkrEval<BaseElement> {
         E::ZERO
     }
 }
+
+// LogUpGkrSimple
+// =================================================================================================
+
+#[derive(Clone, Debug)]
+struct LogUpGkrPeriodic {
+    // dummy main trace
+    main_trace: ColMatrix<BaseElement>,
+    info: TraceInfo,
+}
+
+impl LogUpGkrPeriodic {
+    fn new(trace_len: usize, aux_segment_width: usize) -> Self {
+        assert!(trace_len < u32::MAX.try_into().unwrap());
+
+        let table: Vec<BaseElement> =
+            (0..trace_len).map(|idx| BaseElement::from(idx as u32)).collect();
+        let mut multiplicity: Vec<BaseElement> =
+            (0..trace_len).map(|_idx| BaseElement::ZERO).collect();
+        //multiplicity[0] = BaseElement::new(3 * trace_len as u64 - 3 * 4);
+        //multiplicity[1] = BaseElement::new(3 * 4);
+        multiplicity.iter_mut().step_by(8).for_each(|m| *m = BaseElement::from(3_u32));
+
+        let mut values_0: Vec<BaseElement> = (0..trace_len).map(|_idx| BaseElement::ZERO).collect();
+
+        for i in 0..trace_len / 8 {
+            values_0[8 * i] = BaseElement::from(8 * i as u32);
+        }
+
+        let mut values_1: Vec<BaseElement> = (0..trace_len).map(|_idx| BaseElement::ZERO).collect();
+
+        for i in 0..trace_len / 8 {
+            values_1[8 * i] = BaseElement::from(8 * i as u32);
+        }
+
+        let mut values_2: Vec<BaseElement> = (0..trace_len).map(|_idx| BaseElement::ZERO).collect();
+
+        for i in 0..trace_len / 8 {
+            values_2[8 * i] = BaseElement::from(8 * i as u32);
+        }
+
+        Self {
+            main_trace: ColMatrix::new(vec![table, multiplicity, values_0, values_1, values_2]),
+            info: TraceInfo::new_multi_segment(5, aux_segment_width, 0, trace_len, vec![], true),
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.main_trace.num_rows()
+    }
+}
+
+impl Trace for LogUpGkrPeriodic {
+    type BaseField = BaseElement;
+
+    fn info(&self) -> &TraceInfo {
+        &self.info
+    }
+
+    fn main_segment(&self) -> &ColMatrix<Self::BaseField> {
+        &self.main_trace
+    }
+
+    fn read_main_frame(&self, row_idx: usize, frame: &mut EvaluationFrame<Self::BaseField>) {
+        let next_row_idx = row_idx + 1;
+        self.main_trace.read_row_into(row_idx, frame.current_mut());
+        self.main_trace.read_row_into(next_row_idx % self.len(), frame.next_mut());
+    }
+}
+
+// AIR
+// =================================================================================================
+
+struct LogUpGkrPeriodicAir {
+    context: AirContext<BaseElement>,
+}
+
+impl Air for LogUpGkrPeriodicAir {
+    type BaseField = BaseElement;
+    type PublicInputs = ();
+    type LogUpGkrEvaluator = PeriodicLogUpGkrEval<Self::BaseField>;
+
+    fn new(trace_info: TraceInfo, _pub_inputs: Self::PublicInputs, options: ProofOptions) -> Self {
+        Self {
+            context: AirContext::with_logup_gkr(
+                trace_info,
+                vec![TransitionConstraintDegree::new(1)],
+                vec![],
+                1,
+                0,
+                options,
+            ),
+        }
+    }
+
+    fn context(&self) -> &AirContext<Self::BaseField> {
+        &self.context
+    }
+
+    fn evaluate_transition<E: math::FieldElement<BaseField = Self::BaseField>>(
+        &self,
+        frame: &EvaluationFrame<E>,
+        _periodic_values: &[E],
+        result: &mut [E],
+    ) {
+        let current = frame.current()[0];
+        let next = frame.next()[0];
+
+        // increments by 1
+        result[0] = next - current - E::ONE;
+    }
+
+    fn get_assertions(&self) -> Vec<Assertion<Self::BaseField>> {
+        vec![Assertion::single(0, 0, BaseElement::ZERO)]
+    }
+
+    fn evaluate_aux_transition<F, E>(
+        &self,
+        _main_frame: &EvaluationFrame<F>,
+        _aux_frame: &EvaluationFrame<E>,
+        _periodic_values: &[F],
+        _aux_rand_elements: &AuxRandElements<E>,
+        _result: &mut [E],
+    ) where
+        F: FieldElement<BaseField = Self::BaseField>,
+        E: FieldElement<BaseField = Self::BaseField> + ExtensionOf<F>,
+    {
+        // do nothing
+    }
+
+    fn get_aux_assertions<E: FieldElement<BaseField = Self::BaseField>>(
+        &self,
+        _aux_rand_elements: &AuxRandElements<E>,
+    ) -> Vec<Assertion<E>> {
+        vec![]
+    }
+
+    fn get_logup_gkr_evaluator<E: FieldElement<BaseField = Self::BaseField>>(
+        &self,
+    ) -> Self::LogUpGkrEvaluator {
+        PeriodicLogUpGkrEval::default()
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct PeriodicLogUpGkrEval<B: FieldElement> {
+    _field: PhantomData<B>,
+}
+
+impl LogUpGkrEvaluator for PeriodicLogUpGkrEval<BaseElement> {
+    type BaseField = BaseElement;
+
+    type PublicInputs = ();
+
+    fn get_oracles(&self) -> Vec<LogUpGkrOracle<Self::BaseField>> {
+        let committed_0 = LogUpGkrOracle::CurrentRow(0);
+        let committed_1 = LogUpGkrOracle::CurrentRow(1);
+        let committed_2 = LogUpGkrOracle::CurrentRow(2);
+        let committed_3 = LogUpGkrOracle::CurrentRow(3);
+        let committed_4 = LogUpGkrOracle::CurrentRow(4);
+        let periodic = LogUpGkrOracle::PeriodicValue(vec![
+            Self::BaseField::ONE,
+            Self::BaseField::ZERO,
+            Self::BaseField::ZERO,
+            Self::BaseField::ZERO,
+            Self::BaseField::ZERO,
+            Self::BaseField::ZERO,
+            Self::BaseField::ZERO,
+            Self::BaseField::ZERO,
+        ]);
+        vec![committed_0, committed_1, committed_2, committed_3, committed_4, periodic]
+    }
+
+    fn get_num_rand_values(&self) -> usize {
+        1
+    }
+
+    fn get_num_fractions(&self) -> usize {
+        4
+    }
+
+    fn max_degree(&self) -> usize {
+        3
+    }
+
+    fn build_query<E>(&self, frame: &EvaluationFrame<E>, _periodic_values: &[E]) -> Vec<E>
+    where
+        E: FieldElement<BaseField = Self::BaseField>,
+    {
+        let cur = frame.current();
+        cur.to_vec()
+    }
+
+    fn evaluate_query<F, E>(
+        &self,
+        query: &[F],
+        rand_values: &[E],
+        numerator: &mut [E],
+        denominator: &mut [E],
+    ) where
+        F: FieldElement<BaseField = Self::BaseField>,
+        E: FieldElement<BaseField = Self::BaseField> + ExtensionOf<F>,
+    {
+        assert_eq!(numerator.len(), 4);
+        assert_eq!(denominator.len(), 4);
+        assert_eq!(query.len(), 5);
+        numerator[0] = E::from(query[1]);
+        numerator[1] = E::ONE;
+        numerator[2] = E::ONE;
+        numerator[3] = E::ONE;
+
+        denominator[0] = rand_values[0] - E::from(query[0]);
+        denominator[1] = -(rand_values[0] - E::from(query[2]));
+        denominator[2] = -(rand_values[0] - E::from(query[3]));
+        denominator[3] = -(rand_values[0] - E::from(query[4]));
+    }
+
+    fn compute_claim<E>(&self, _inputs: &Self::PublicInputs, _rand_values: &[E]) -> E
+    where
+        E: FieldElement<BaseField = Self::BaseField>,
+    {
+        E::ZERO
+    }
+}
+
 // Prover
 // ================================================================================================
 
