b36eaa3f7d3360bdfaffe6a642431bf0c42c42da
Al-Kindi-0 <82364884+Al-Kindi-0@users.noreply.github.com>
2024-08-23 12:55:57 +0200

    chore: fix clippy warnings

--- examples/src/lamport/aggregate/air.rs
+++ examples/src/lamport/aggregate/air.rs
@@ -3,9 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-use std::marker::PhantomData;
-
-use air::LogUpGkrEvaluator;
 use core_utils::flatten_slice_elements;
 use winterfell::{
     math::{fields::f128::BaseElement, FieldElement, ToElements},
