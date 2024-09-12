48a1d6f6e92b2c842d188172716cb157a8b4dfa5
Al-Kindi-0 <82364884+Al-Kindi-0@users.noreply.github.com>
2024-09-12 11:01:36 +0200

    Fix degree checks (#312)
    * feat: relax constraint degree checks in debug mode
    * fix: remove equality test and restore evaluation domain sizes check

--- prover/src/constraints/evaluation_table.rs
+++ prover/src/constraints/evaluation_table.rs
@@ -210,11 +210,13 @@ impl<'a, E: FieldElement> ConstraintEvaluationTable<'a, E> {
             max_degree = core::cmp::max(max_degree, degree);
         }
 
-        // make sure expected and actual degrees are equal
-        assert_eq!(
-            self.expected_transition_degrees, actual_degrees,
-            "transition constraint degrees didn't match\nexpected: {:>3?}\nactual:   {:>3?}",
-            self.expected_transition_degrees, actual_degrees
+        // make sure the actual degrees are less than or equal to the expected degree bounds
+        assert!(
+            self.expected_transition_degrees >= actual_degrees,
+            "transition constraint degrees do not satisfy the expected degree bounds
+             \nexpected degree bounds: {:>3?}\nactual degrees:   {:>3?}",
+            self.expected_transition_degrees,
+            actual_degrees
         );
 
         // make sure the actual degrees are less than or equal to the expected degree bounds
